3b2ed7c9972f2e7de85858e2b1406eb017f33edc
Damon.chen <someday8701@163.com>
2026-01-11 11:09:30 +0800

    update config 21

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { handle } from "hono/vercel";
 import authRoutes from "./src/routes/auth.js";
 import passwordRoutes from "./src/routes/passwords.js";
 
-const app = new Hono().basePath("/api");
+const app = new Hono().basePath("/");
 app.get("/", (c) => {
   return c.text("Damon love Ella & Xiaoqun");
 });
