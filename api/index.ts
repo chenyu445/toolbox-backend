38ea1450bcb66a643ed9806bb1fd074b98b92df7
Damon.chen <someday8701@163.com>
2026-01-11 09:57:27 +0800

    update config 18

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,7 @@
-import app from "../index.js";
-export default app;
+/**
+ * @file index.ts
+ * @description This file is the entry point of the application.
+ */
+// import app from "../index.js";
+// export default app;
+console.log("hello world");
